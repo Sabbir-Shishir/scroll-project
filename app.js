d33e5b75a5e2d1292553ed959118387d0d74ee7f
Sabbir-Shishir <sabbirshishir21@gmail.com>
2023-05-20 23:38:46 +0600

    project completed

--- app.js
+++ app.js
@@ -3,11 +3,77 @@
 // slice extracts a section of a string without modifying original string
 //offsetTop - A Number, representing the top position of the element, in pixels
 
-// ********** set date ************
+// ! set date
+const date = document.getElementById('date');
+date.innerHTML = new Date().getFullYear();
 
-// ********** close links ************
+// ! close links 
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
 
-// ********** fixed navbar ************
+navToggle.addEventListener('click', () => {
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const linksHeight = links.getBoundingClientRect().height;
 
-// ********** smooth scroll ************
-// select links
+    if (containerHeight === 0) {
+        linksContainer.style.height = `${linksHeight}px`
+    } else {
+        linksContainer.style.height = 0;
+    }
+})
+
+// ! fixed navbar 
+const navbar = document.getElementById('nav');
+const topLink = document.querySelector('.top-link')
+
+window.addEventListener('scroll', () => {
+    const scrollHeight = window.scrollY;
+    const navHeight = navbar.getBoundingClientRect().height;
+
+    if (scrollHeight > navHeight) {
+        navbar.classList.add('fixed-nav');
+    } else {
+        navbar.classList.remove('fixed-nav')
+    }
+
+    if (scrollHeight > 500) {
+        topLink.classList.add('show-link');
+    } else {
+        topLink.classList.remove('show-link');
+    }
+})
+
+// ! smooth scroll
+
+const scrollLinks = document.querySelectorAll('.scroll-link');
+
+scrollLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+        // * prevent default
+        e.preventDefault();
+
+        // * navigate to specific spot
+        const id = e.currentTarget.getAttribute('href').slice(1);
+        const element = document.getElementById(id);
+
+        // * calculate the heigts
+        const navHeight = navbar.getBoundingClientRect().height;
+        const containerHeight = linksContainer.getBoundingClientRect().height;
+        const fixedNav = navbar.classList.contains('fixed-nav');
+        let position = element.offsetTop - navHeight;
+
+        if (!fixedNav) {
+            position = position - navHeight;
+        } 
+
+        if (navHeight > 82) {
+            position = position + containerHeight;
+        }
+
+        window.scrollTo({
+            left:0, top: position,
+        });
+        linksContainer.style.height = 0;
+    });
+});
